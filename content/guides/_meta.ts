bc4c207671a126a03cb0773ca153826fc9004f52
nobody <grocerpublishagent@peoplesgrocers.com>
2025-09-23 17:59:03 -0700

    feat: try to quickly explain Happy Coder features

--- content/guides/_meta.ts
+++ content/guides/_meta.ts
@@ -1,3 +1,9 @@
 export default {
   'push-notifications': 'Enable Push Notifications',
+  'happy-coder-best-practices': {
+    title: 'Happy Coder: Best Practices for agentic coding',
+    theme: {
+      typesetting: 'article'
+    }
+  },
 }
